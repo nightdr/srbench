77053cc8645af4cb4b61c11715347dc96fae55e6
David Randall <madmathskills@gmail.com>
2022-03-20 02:51:01 -0600

    tuned model for ground-truth problems

---
+++ experiment/methods/tuned/params/_bingoregressor.py
@@ -0,0 +1,31 @@
+from bingo.evolutionary_algorithms.age_fitness import AgeFitnessEA
+from bingo.evolutionary_optimizers.fitness_predictor_island import FitnessPredictorIsland
+
+params = {
+    "clo_alg": "lm",
+    "clo_threshold": 1e-05,
+    "crossover_prob": 0.3,
+    "evolutionary_algorithm": AgeFitnessEA,
+    "fitness_threshold": 1e-16,
+    "generations": 1000000000000000019884624838656,
+    "island": FitnessPredictorIsland,
+    "max_evals": 500000,
+    "max_time": 7200,
+    "metric": "mse",
+    "mutation_prob": 0.45,
+    "operators": [
+        "+",
+        "-",
+        "*",
+        "/",
+        "sin",
+        "cos",
+        "exp",
+        "log",
+        "sqrt"  # NOTE: added sqrt
+    ],
+    "parallel": False,
+    "population_size": 500,
+    "stack_size": 32,
+    "use_simplification": True
+}
