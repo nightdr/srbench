5336a799c8866dd8133561493449e56205afba91
David Randall <madmathskills@gmail.com>
2022-05-14 21:46:08 -0600

    added mpi4py to conda dependencies and added tag checkouts

--- submission/Bingo/install.sh
+++ submission/Bingo/install.sh
@@ -3,13 +3,15 @@ git clone --recurse-submodules https://github.com/nightdr/bingo.git
 cd bingo
 
 # checkout srbench bingo
-git checkout srbench
+git checkout tags/srbench_bingo -b srbench_competition
+#git checkout srbench
 
-# checkout develop bingocpp
+# checkout srbench bingocpp
 cd bingocpp
 git remote add nightdr https://github.com/nightdr/bingocpp.git
 git fetch nightdr
-git checkout -b nightdr_dev nightdr/develop
+git checkout tags/srbench_bingocpp -b srbench_competition
+#git checkout -b nightdr_dev nightdr/develop
 cd ..
 
 # install
